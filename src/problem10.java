40391329ae9540c843245726fe964fcd21a036ce
debanshu78 <42779312+debanshu78@users.noreply.github.com>
2019-08-22 21:15:19 +0530

    v

--- src/problem10.java
+++ src/problem10.java
@@ -4,28 +4,33 @@ Find the sum of all the primes below two million.
 
 */
 
-
 public class problem10 {
+	static boolean primchek(long n)
+	{
+		boolean t=true;
+	if(n%2==0)
+	{
+		return false;
+	}
+	for(long i=3;i*i<=n;i=i+2)
+	{
+		if(n%i==0)
+		{
+			return false;
+		}
+	}
+	return true;
+	}
+	
 	public static void sumPrime(int n)
 	{
 		long sum=2;
 		long i=3;
-	    int count=2;
-	    while(i<2000000)
+	    while(i<n)
 	    {
-	    	boolean count1=true;
-	    	for(int j=2;j<=Math.sqrt(i)+1;j++)
-	    	{
-	    		if(i%j==0)
-	    		{
-	    			count1=false;
-	    			break;
-	    		}
-	    	}
-	    	if(count1==true)
+	    	if(primchek(i))
 	    	{
 	    		sum=sum+i;
-	    		count++;
 	    	}
 	    	i=i+2;
 	    }
@@ -34,7 +39,7 @@ public class problem10 {
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-sumPrime(10);
+sumPrime(2000000);
 	}
 
 }
